c1c89328a053f8ddf22e2ff5888d603f7cc76e09
Javier Cala Uribe <javier@arkelao.co>
2014-09-11 09:20:31 -0500

    Improvement the implementation of PrivateIP

--- Classes/PrivateIp.m
+++ Classes/PrivateIp.m
@@ -8,16 +8,44 @@
 
 #import "PrivateIp.h"
 #import "UIDevice-Reachability.h"
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 
 @implementation PrivateIp
 
 - (void) get {
-    NSString* localWiFiIPAddress = [[UIDevice currentDevice] localWiFiIPAddress] != NULL ? [[UIDevice currentDevice] localWiFiIPAddress] :@"0.0.0.0";
+    NSString* localWiFiIPAddress = [self getIPAddress];
     [super sendHttp:[super createResponseFromString:localWiFiIPAddress withKey:[self getName]]];
 }
 
 - (NSString *) getName {
 	return @"private_ip";
+}
+
+- (NSString *)getIPAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
 }
 
 @end
