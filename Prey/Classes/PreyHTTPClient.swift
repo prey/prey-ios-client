4412cd471ca80b9666ac82bfa3e314048b247a43
Javier Cala <javier@preyhq.com>
2016-03-23 16:25:42 -0300

    Add PreyHTTPClient class

--- Prey/Classes/PreyHTTPClient.swift
+++ Prey/Classes/PreyHTTPClient.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+import UIKit
+
+class PreyHTTPClient {
+    
+    // MARK: Properties
+    
+    static let sharedInstance = PreyHTTPClient()
+    private init() {
+    }
+    
+    // Define UserAgent
+    var userAgent : String {
+        let systemVersion = UIDevice.currentDevice().systemVersion
+        let appVersion    = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as! String
+        return "Prey/\(appVersion) (iOS \(systemVersion))"
+    }
+
+    // Define NSURLSessionConfiguration
+    func getSessionConfig(authString: String) -> NSURLSessionConfiguration {
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        sessionConfig.HTTPAdditionalHeaders = ["User-Agent" : userAgent, "Content-Type" : "application/json", "Authorization" : authString]
+        return sessionConfig
+    }
+
+    // Encode Authorization for HTTP Header
+    func encodeAuthorization(authString: String) -> String {
+        let userAuthorizationData   = authString.dataUsingEncoding(NSUTF8StringEncoding)
+        let encodedCredential       = userAuthorizationData!.base64EncodedStringWithOptions([])
+        return "Basic \(encodedCredential)"
+    }
+    
+    // Define CompletionHandler
+    func getCompletionHandler(withCompletion:(dataRequest: NSData?, responseRequest: NSURLResponse?, error: NSError?) -> Void) -> (NSData?, NSURLResponse?, NSError?) -> Void {
+
+        let completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            withCompletion(dataRequest:data, responseRequest:response, error:error)
+        }
+        return completionHandler
+    }
+
+    
+    // MARK: Requests to Prey API
+    
+    // LogIn User to Control Panel
+    func userLogInToPrey(reload: Int, preyUser: PreyUser, onCompletion:(dataRequest: NSData?, responseRequest: NSURLResponse?, error: NSError?)->Void) {
+        
+        // If userApiKey is empty select userEmail
+        let username = (PreyConfig.sharedInstance.userApiKey != nil) ? PreyConfig.sharedInstance.userApiKey : preyUser.email
+        
+        // Encode username and pwd
+        let userAuthorization = encodeAuthorization(NSString(format:"%@:%@", username!, preyUser.password!) as String)
+
+        // Set session Config
+        let sessionConfig   = getSessionConfig(userAuthorization)
+        let session         = NSURLSession(configuration: sessionConfig)
+        
+        // Set Endpoint
+        let requestURL      = NSURL(string: URLControlPanel.stringByAppendingString(logInEndpoint))
+        let request         = NSMutableURLRequest(URL:requestURL!)
+        request.HTTPMethod  = Method.GET.rawValue
+    
+        // Prepare Request to Send
+        let task = session.dataTaskWithRequest(request, completionHandler:getCompletionHandler(onCompletion))
+     
+        // Send Request
+        task.resume()
+    }
+}
