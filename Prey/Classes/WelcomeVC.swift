c92c9e105adc19064e8b667b0e5bdd9da1284499
Javier Cala Uribe <javier@arkelao.co>
2016-07-20 15:22:09 -0400

    Add images to welcomeVC

--- Prey/Classes/WelcomeVC.swift
+++ Prey/Classes/WelcomeVC.swift
@@ -30,5 +30,53 @@ class WelcomeVC: UIViewController{
 
         super.viewWillAppear(animated)
     }
+    
+    // MARK: Actions
+    
+    // Show SignUp view
+    @IBAction func showSignUpVC(sender: UIButton) {
+        
+        // Get SharedApplication delegate
+        guard let appWindow = UIApplication.sharedApplication().delegate?.window else {
+            print("error with sharedApplication")
+            return
+        }
+        
+        // Get SignUpVC from Storyboard
+        if let controller:UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier(StoryboardIdVC.signUp.rawValue) {
+            
+            // Set controller to rootViewController
+            let navigationController:UINavigationController = appWindow!.rootViewController as! UINavigationController
+            
+            let transition:CATransition = CATransition()
+            transition.type = kCATransitionFade
+            navigationController.view.layer.addAnimation(transition, forKey: "")
+            
+            navigationController.setViewControllers([controller], animated: false)
+        }
+    }
+    
+    // Show SignIn view
+    @IBAction func showSignInVC(sender: UIButton) {
+        
+        // Get SharedApplication delegate
+        guard let appWindow = UIApplication.sharedApplication().delegate?.window else {
+            print("error with sharedApplication")
+            return
+        }
+        
+        // Get SignInVC from Storyboard
+        if let controller:UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier(StoryboardIdVC.signIn.rawValue) {
+            
+            // Set controller to rootViewController
+            let navigationController:UINavigationController = appWindow!.rootViewController as! UINavigationController
+            
+            let transition:CATransition = CATransition()
+            transition.type = kCATransitionFade
+            navigationController.view.layer.addAnimation(transition, forKey: "")
+            
+            navigationController.setViewControllers([controller], animated: false)
+        }
+    }
 }
 
