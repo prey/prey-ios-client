15a1955f350cae3b0b9a49312494e91d72fbaf6c
Javier Cala Uribe <javier@arkelao.co>
2016-08-08 16:06:04 -0400

    Add UnitTests for PreyRestTests

--- PreyTests/PreyRestTests.swift
+++ PreyTests/PreyRestTests.swift
@@ -115,7 +115,7 @@ class PreyRestTests: XCTestCase {
         
         let expectation     = self.expectationWithDescription("Expecta Test: Check Status")
         
-        let actionDeviceResponse: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
             
             // Error is nil
             XCTAssertNil(error)
@@ -133,12 +133,244 @@ class PreyRestTests: XCTestCase {
 
         // Check userApiKey isn't empty
         if let username = PreyConfig.sharedInstance.userApiKey {
-            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password: "x", params: nil, httpMethod:Method.GET.rawValue, endPoint:actionsDeviceEndpoint , onCompletion:actionDeviceResponse)
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password: "x", params: nil, httpMethod:Method.GET.rawValue, endPoint:actionsDeviceEndpoint , onCompletion:response)
         } else {
             // Check if apiKey is nil
             XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
         }        
         
         self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+    
+    // Test NotificationId
+    func testRest06CheckNotificationId() {
+        
+        let expectation     = self.expectationWithDescription("Expecta Test: Check NotificationId")
+    
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            
+            // Error is nil
+            XCTAssertNil(error)
+            
+            let httpURLResponse = response as! NSHTTPURLResponse
+            
+            XCTAssertEqual(httpURLResponse.statusCode,200)
+            
+            expectation.fulfill()
+        }
+        
+        let params:[String: AnyObject] = ["notification_id" : "t3stT0k3n"]
+        
+        // Check userApiKey isn't empty
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password:"x", params:params, httpMethod:Method.POST.rawValue, endPoint:dataDeviceEndpoint, onCompletion:response)
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+    
+    // Test Transaction InAppPurchase
+    func testRest07TransactionInAppPurchase() {
+        
+        let expectation                 = self.expectationWithDescription("Expecta Test: Transaction InAppPurchase")
+        
+        let receipt                     = "t3stT0k3n".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)! as NSData
+        let receiptDataString           = receipt.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+        
+        let params:[String: AnyObject]  = ["receipt-data" : receiptDataString]
+
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            
+            // Error is nil
+            XCTAssertNil(error)
+            
+            let httpURLResponse = response as! NSHTTPURLResponse
+            
+            XCTAssertEqual(httpURLResponse.statusCode,403)
+            
+            expectation.fulfill()
+        }
+        
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password:"x", params:params, httpMethod:Method.POST.rawValue, endPoint:subscriptionEndpoint, onCompletion:response)
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+    
+    // Test Check Geofence
+    func testRest08CheckGeofences() {
+        
+        let expectation     = self.expectationWithDescription("Expecta Test: Check Geofence")
+        
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            
+            // Error is nil
+            XCTAssertNil(error)
+            
+            let httpURLResponse = response as! NSHTTPURLResponse
+            
+            XCTAssertEqual(httpURLResponse.statusCode,200)
+            
+            expectation.fulfill()
+        }
+        
+        // Check userApiKey isn't empty
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password:"x", params:nil, httpMethod:Method.GET.rawValue, endPoint:geofencingEndpoint, onCompletion:response)
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+
+    // Test Check Geofence
+    func testRest09SendEvent() {
+        
+        let expectation     = self.expectationWithDescription("Expecta Test: Send Events")
+        
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            
+            // Error is nil
+            XCTAssertNil(error)
+            
+            let httpURLResponse = response as! NSHTTPURLResponse
+            
+            XCTAssertEqual(httpURLResponse.statusCode,200)
+            
+            expectation.fulfill()
+        }
+        
+        let regionInfo:[String: AnyObject] = [
+            kGeofence.ZONEID.rawValue       : "testZone",
+            kLocation.lng.rawValue          : 0,
+            kLocation.lat.rawValue          : 0,
+            kLocation.accuracy.rawValue     : 0,
+            kLocation.method.rawValue       : "native"]
+        
+        let params:[String: AnyObject] = [
+            kGeofence.INFO.rawValue         : regionInfo,
+            kGeofence.NAME.rawValue         : "testZone"]
+
+        
+        // Check userApiKey isn't empty
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password:"x", params:params, httpMethod:Method.POST.rawValue, endPoint:eventsDeviceEndpoint, onCompletion:response)
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+    
+    // Test Response Device
+    func testRest10ResponseDevice() {
+        
+        let expectation     = self.expectationWithDescription("Expecta Test: Response Device")
+
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            
+            // Error is nil
+            XCTAssertNil(error)
+            
+            let httpURLResponse = response as! NSHTTPURLResponse
+            
+            XCTAssertEqual(httpURLResponse.statusCode,200)
+            
+            expectation.fulfill()
+        }
+
+        // Params struct
+        let params:[String: AnyObject] = [
+            kData.status.rawValue   : kStatus.started.rawValue,
+            kData.target.rawValue   : kAction.camouflage.rawValue,
+            kData.command.rawValue  : kCommand.start.rawValue]
+        
+        // Send info to panel
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password:"x", params:params, httpMethod:Method.POST.rawValue, endPoint:responseDeviceEndpoint, onCompletion:response)
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+    
+    // Test Send Report
+    func testRest11SendReport() {
+        
+        let expectation     = self.expectationWithDescription("Expecta Test: Response Device")
+        
+        let response: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
+            
+            // Error is nil
+            XCTAssertNil(error)
+            
+            let httpURLResponse = response as! NSHTTPURLResponse
+            
+            XCTAssertEqual(httpURLResponse.statusCode,409) // Device isn't missing on web panel
+            
+            expectation.fulfill()
+        }
+        
+        let reportData   = NSMutableDictionary()
+        let reportImages = NSMutableDictionary()
+        
+        // Params struct
+        let params:[String : AnyObject] = [
+            kReportLocation.LONGITURE.rawValue    : 0,
+            kReportLocation.LATITUDE.rawValue     : 0,
+            kReportLocation.ALTITUDE.rawValue     : 0,
+            kReportLocation.ACCURACY.rawValue     : 0]
+        
+        // Save location to reportData
+        reportData.addEntriesFromDictionary(params)
+        
+        // Send info to panel
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            PreyHTTPClient.sharedInstance.sendDataReportToPrey(username, password:"x", params:reportData, images:reportImages, httpMethod:Method.POST.rawValue, endPoint:reportDataDeviceEndpoint, onCompletion:response)
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(15, handler:nil)
+    }
+    
+    // Test Delete Device
+    func testRest12DeleteDevice() {
+        
+        let expectation     = self.expectationWithDescription("Expecta Test: Delete Device")
+        
+        // Check if deviceKey is nil
+        XCTAssertNotNil(PreyConfig.sharedInstance.deviceKey)        
+        
+        // Check userApiKey isn't empty
+        if let username = PreyConfig.sharedInstance.userApiKey {
+            
+            PreyHTTPClient.sharedInstance.userRegisterToPrey(username, password:"x", params:nil, httpMethod:Method.DELETE.rawValue, endPoint:deleteDeviceEndpoint, onCompletion:PreyHTTPResponse.checkResponse(RequestType.DeleteDevice, onCompletion:{(isSuccess: Bool) in
+                
+                // Check if add device is success
+                XCTAssertTrue(isSuccess)
+                
+                expectation.fulfill()
+            }))
+            
+        } else {
+            // Check if apiKey is nil
+            XCTAssertNotNil(PreyConfig.sharedInstance.userApiKey)
+        }
+        
+        self.waitForExpectationsWithTimeout(60, handler:nil)
     }
 }
