d220568d2c6db9041fb3377f1bd12b98eb5f9916
Javier Cala Uribe <javier@arkelao.co>
2018-10-25 09:54:50 -0500

    Fix issue on Swifter with DispatchQueue

--- Prey/Swifter/HttpServerIO.swift
+++ Prey/Swifter/HttpServerIO.swift
@@ -69,20 +69,17 @@ public class HttpServerIO {
     }
 
     @available(macOS 10.10, *)
-    public func start(_ port: in_port_t = 8080, forceIPv4: Bool = false, priority: DispatchQueue.GlobalQueuePriority = DispatchQueue.GlobalQueuePriority.background) throws {
-    //public func start(_ port: in_port_t = 8080, forceIPv4: Bool = false, priority: DispatchQoS.QoSClass = DispatchQoS.QoSClass.background) throws {
+    public func start(_ port: in_port_t = 8080, forceIPv4: Bool = false, priority: DispatchQoS.QoSClass = DispatchQoS.QoSClass.background) throws {
         guard !self.operating else { return }
         stop()
         self.state = .starting
         let address = forceIPv4 ? listenAddressIPv4 : listenAddressIPv6
         self.socket = try Socket.tcpSocketForListen(port, forceIPv4, SOMAXCONN, address)
-        DispatchQueue.global(priority: priority).async { [weak self] in
-        //DispatchQueue.global(qos: priority).async { [weak self] in
+        DispatchQueue.global(qos: priority).async { [weak self] in
             guard let strongSelf = self else { return }
             guard strongSelf.operating else { return }
             while let socket = try? strongSelf.socket.acceptClientSocket() {
-                DispatchQueue.global(priority: priority).async { [weak self] in
-                //DispatchQueue.global(qos: priority).async { [weak self] in
+                DispatchQueue.global(qos: priority).async { [weak self] in
                     guard let strongSelf = self else { return }
                     guard strongSelf.operating else { return }
                     strongSelf.queue.async {
