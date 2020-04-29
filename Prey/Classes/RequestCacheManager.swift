a29ee6cdccb5a02e9679fd1761c8e5130c05dfe8
Javier Cala Uribe <javier@arkelao.co>
2020-04-29 11:10:05 -0500

    Add RequestCache on CoreData model

---
+++ Prey/Classes/RequestCacheManager.swift
@@ -0,0 +1,21 @@
+//
+//  RequestCacheManager.swift
+//  Prey
+//
+//  Created by Javier Cala Uribe on 29/4/20.
+//  Copyright © 2020 Fork Ltd. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class RequestCacheManager:NSObject {
+
+    // MARK: Properties
+
+    static let sharedInstance = RequestCacheManager()
+    override fileprivate init() {
+    }
+    
+    
+}
---
+++ Prey/RequestCache+Class.swift
@@ -0,0 +1,16 @@
+//
+//  RequestCache+CoreDataClass.swift
+//  Prey
+//
+//  Created by Javier Cala Uribe on 29/4/20.
+//  Copyright © 2020 Fork Ltd. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class RequestCache: NSManagedObject {
+
+}
