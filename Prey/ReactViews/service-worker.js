37e9d114754aa7853f1d13caf264cd52dff70ba0
Patricio Jofre <luis.jofre.g@gmail.com>
2021-10-06 22:55:39 -0300

    fix appstore connect issue. change button ok to next

--- Prey/ReactViews/service-worker.js
+++ Prey/ReactViews/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.24f17145000d0328761fc196ebc166c4.js"
+  "./precache-manifest.41ef2425811d19ead04ce9cfcfce9742.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
