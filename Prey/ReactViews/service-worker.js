855dc4da5791621fc378227c857482798f30cf35
Orlando Aliaga <oaliaga@gmail.com>
2023-07-13 03:21:15 -0400

    New style
    New style

--- Prey/ReactViews/service-worker.js
+++ Prey/ReactViews/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7fd616e7ea7ede2fed1402e690acdeaa.js"
+  "./precache-manifest.736d13edafbf5cd2184cf3a722377c91.js"
 );
 
 workbox.clientsClaim();
