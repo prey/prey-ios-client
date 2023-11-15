a70a2e2542725aa04ab218ea4ce9ed7386108c90
Pato Jofre <luis.jofre.g@gmail.com>
2023-11-15 15:36:10 +0000

    UNNotificationServiceExtension

--- PreyNotify/NotificationViewController.swift
+++ PreyNotify/NotificationViewController.swift
@@ -14,15 +14,21 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
 
     @IBOutlet var label: UILabel?
     @IBOutlet var alertTitle: UILabel?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
         self.preferredContentSize = CGSize(width: self.view.frame.width, height: 200.0)
     }
-    
+
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = notification.request.content.body
+        // Handle the modified content received from the service extension
+        let modifiedContent = notification.request.content
+
+        // self.label?.text = notification.request.content.body
+        
+        // solo para probar, acá le pasamos el content nuevo desde la clase NotificationService
+        self.label?.text = modifiedContent.body
         self.alertTitle?.text = "IMPORTANT ALERT".localized
     }
 }
---
+++ PreyNotify/UNNotificationServiceExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UNNotificationServiceExtension.swift
+//  PreyNotify
+//
+//  Created by Pato Jofre on 15-11-23.
+//  Copyright © 2023 Prey, Inc. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        // Process the notification content here
+
+        // Create a mutable copy of the original content
+        let updatedContent = (request.content.mutableCopy() as? UNMutableNotificationContent) ?? UNMutableNotificationContent()
+
+        // Modify the content (example: update the title)
+        updatedContent.title = "New Title: \(request.content.title)"
+
+        // Call the contentHandler with the updated content
+        contentHandler(updatedContent)
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // You may use this method to deliver a truncated version of the notification.
+    }
+}
