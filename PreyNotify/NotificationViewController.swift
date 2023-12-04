79f8eb2b610772c2fc8b72ff9d555760520401d7
Pato Jofre <luis.jofre.g@gmail.com>
2023-12-04 22:40:28 +0000

    Revert "UNNotificationServiceExtension"
    This reverts commit a70a2e2542725aa04ab218ea4ce9ed7386108c90.

--- PreyNotify/NotificationViewController.swift
+++ PreyNotify/NotificationViewController.swift
@@ -14,21 +14,15 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
 
     @IBOutlet var label: UILabel?
     @IBOutlet var alertTitle: UILabel?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
         self.preferredContentSize = CGSize(width: self.view.frame.width, height: 200.0)
     }
-
+    
     func didReceive(_ notification: UNNotification) {
-        // Handle the modified content received from the service extension
-        let modifiedContent = notification.request.content
-
-        // self.label?.text = notification.request.content.body
-        
-        // solo para probar, acá le pasamos el content nuevo desde la clase NotificationService
-        self.label?.text = modifiedContent.body
+        self.label?.text = notification.request.content.body
         self.alertTitle?.text = "IMPORTANT ALERT".localized
     }
 }
