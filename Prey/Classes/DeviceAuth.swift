f818111242d43d690b2e2644f3858bf4b43cd237
Pato Jofre <luis.jofre.g@gmail.com>
2025-08-29 12:07:04 +0100

    less frequency

--- Prey/Classes/DeviceAuth.swift
+++ Prey/Classes/DeviceAuth.swift
@@ -284,8 +284,8 @@ class DeviceAuth: NSObject, UIAlertViewDelegate, CLLocationManagerDelegate {
     // Independent action check scheduler
     private static var actionsCheckTimer: Timer?
     private static var deviceStatusTimer: Timer?
-    private static let actionsCheckInterval: TimeInterval = 5 * 60 // 5 minutes
-    private static let deviceStatusCheckInterval: TimeInterval = 10 * 60 // 10 minutes
+    private static let actionsCheckInterval: TimeInterval = 30 * 60 // 30 minutes
+    private static let deviceStatusCheckInterval: TimeInterval = 30 * 60 // 30 minutes
     
     // Location delegates for consolidated location management
     private var locationDelegates: [LocationDelegate] = []
